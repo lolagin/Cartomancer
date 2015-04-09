2e8ebb26a3ba2631824a594c867bcb28eeb3aa25
Jeffrey Rosenthal <jeffrey.rosenthal@gmail.com>
2015-04-09 08:01:15 -0700

    hey buddy

---
+++ DefListTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  DefListTableViewController.h
+//  Cartomancer
+//
+//  Created by Jeffrey C Rosenthal on 4/8/15.
+//  Copyright (c) 2015 northtwilight_development. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DefListTableViewController : UITableViewController
++(instancetype)preLoadedView:(NSArray *)reading;
+@end
