03922c9982767733054c999469c5b18bfa1df057
Massimo Savino <north.twilight@gmail.com>
2015-04-09 21:31:51 -0700

    Marseilles images loaded

---
+++ Cartomancer/IntroViewController.h
@@ -0,0 +1,13 @@
+//
+//  IntroViewController.h
+//  Cartomancer
+//
+//  Created by Massimo Savino on 2015-04-09.
+//  Copyright (c) 2015 northtwilight_development. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface IntroViewController : UIViewController
+
+@end
